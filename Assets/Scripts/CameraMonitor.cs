ceebf9ba11e9769af8e28ae0ff0fa9dc6253e66e
Alan Sartorio <alan42ga@hotmail.com>
2023-09-01 01:22:35 -0300

    Use emissionMap instead of baseColor for monitor screens

--- Assets/Scripts/CameraMonitor.cs
+++ Assets/Scripts/CameraMonitor.cs
@@ -7,10 +7,7 @@ public class CameraMonitor : MonoBehaviour
     {
         Texture rt = securityCamera.GetComponent<CreateRenderTexture>().rt;
         Material material = GetComponent<Renderer>().material;
-        material.mainTexture = rt;
-        material.color = Color.white;
-        // material.SetColor("_EmissionColor", Color.white);
-        // material.SetTexture("_EmissionMap", rt);
-        // material.EnableKeyword("_EMISSION");
+        // material.mainTexture = rt;
+        material.SetTexture("_EmissionMap", rt);
     }
 }
