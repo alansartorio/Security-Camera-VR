04338ef545a11ce805899dbb8b7edd7e09738303
Alan Sartorio <alan42ga@hotmail.com>
2025-02-05 13:16:21 -0300

    Fix client color randomization

--- Assets/Scripts/ClientColorRandomizer.cs
+++ Assets/Scripts/ClientColorRandomizer.cs
@@ -1,14 +1,19 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ClientColorRandomizer : MonoBehaviour
 {
-    [SerializeField] private Material skin;
-    [SerializeField] private Material pants;
-    [SerializeField] private Material shirt;
-    [SerializeField] private Material shoes;
+    [SerializeField] private Renderer personRenderer;
 
     private void Awake()
     {
+        var materials = personRenderer.materials.ToDictionary(m => m.name);
+        var skin = materials.GetValueOrDefault("Skin (Instance)")!;
+        var pants = materials.GetValueOrDefault("Pants (Instance)")!;
+        var shirt = materials.GetValueOrDefault("Shirt (Instance)")!;
+        var shoes = materials.GetValueOrDefault("Shoes (Instance)")!;
+
         shirt.color = Random.ColorHSV(0, 1, 0.5f, 1, 0.5f, 1);
         pants.color = Random.ColorHSV(0, 1, 0.5f, 1, 0, shirt.color.grayscale);
         shoes.color = Random.ColorHSV(0, 1, 0.5f, 1, 0, pants.color.grayscale);
